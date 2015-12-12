f62608e012d9562c884a1cc3d33e3bb013b4c99e
Rushi Agrawal <rushi.agr@gmail.com>
2015-12-12 17:19:44 +0530

    Revert "try if alias can be set from current session itself"
    This reverts commit 6da366e45f6e01587c396a5bb42cdd1201cd4538.

--- scripts/mysql-pretty-table.sh
+++ scripts/mysql-pretty-table.sh
@@ -31,7 +31,6 @@ main() {
 # Alias to make mysql tables more usable
 alias mysql='mysql --pager="less --chop-long-lines --quit-if-one-screen --no-init"'
 EOF
-alias mysql='mysql --pager="less --chop-long-lines --quit-if-one-screen --no-init"'
   fi
 
   # Create ~/.lesskey file if not already present
